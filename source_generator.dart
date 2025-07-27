452cab6dc5dd7c56f83ce69db250d5cc90cac192
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2025-07-27 18:01:12 +0530

    Update source_generator.dart

--- source_generator.dart
+++ source_generator.dart
@@ -2,35 +2,13 @@ import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 import 'dart/anime/anime_source_list.dart';
-import 'dart/manga/manga_source_list.dart';
-import 'dart/novel/novel_source_list.dart';
 import 'model/source.dart';
 
 void main() {
   final jsSources = _searchJsSources(Directory("javascript"));
-  genManga(
-    jsSources.where((element) => element.itemType!.name == "manga").toList(),
-  );
   genAnime(
     jsSources.where((element) => element.itemType!.name == "anime").toList(),
   );
-  genNovel(
-    jsSources.where((element) => element.itemType!.name == "novel").toList(),
-  );
-}
-
-void genManga(List<Source> jsMangasourceList) {
-  List<Source> mangaSources = [];
-  mangaSources.addAll(dartMangasourceList);
-  mangaSources.addAll(jsMangasourceList);
-  final List<Map<String, dynamic>> jsonList =
-      mangaSources.map((source) => source.toJson()).toList();
-  final jsonString = jsonEncode(jsonList);
-
-  final file = File('index.json');
-  file.writeAsStringSync(jsonString);
-
-  log('JSON file created: ${file.path}');
 }
 
 void genAnime(List<Source> jsAnimesourceList) {
@@ -42,20 +20,6 @@ void genAnime(List<Source> jsAnimesourceList) {
   final jsonString = jsonEncode(jsonList);
 
   final file = File('anime_index.json');
-  file.writeAsStringSync(jsonString);
-
-  log('JSON file created: ${file.path}');
-}
-
-void genNovel(List<Source> jsNovelSourceList) {
-  List<Source> novelSources = [];
-  novelSources.addAll(dartNovelSourceList);
-  novelSources.addAll(jsNovelSourceList);
-  final List<Map<String, dynamic>> jsonList =
-      novelSources.map((source) => source.toJson()).toList();
-  final jsonString = jsonEncode(jsonList);
-
-  final file = File('novel_index.json');
   file.writeAsStringSync(jsonString);
 
   log('JSON file created: ${file.path}');
